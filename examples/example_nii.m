c2af5ed9f4bc46377f4e50a6cbd1c1a0cfaa73c9
Christopher Madan <cMadan@users.noreply.github.com>
2018-06-05 16:36:52 +0300

    Improved documentation for example_nii code

--- examples/example_nii.m
+++ examples/example_nii.m
@@ -15,13 +15,21 @@ nii = load_nii('example_Sbunny.nii.gz');
 V   = nii.img;
 
 %% select voxels to be part of region label
+% make sure this value is one of the regions included in the intensity coding of the parcellated/segmented image
+% e.g., confirm with "unique(V)"
+% if this region code doesn't exist, the toolbox will enter debug mode and say the region wasn't found
 vol = V == 1;
 
-%% calcFD
+%% do calcFD
+% add the calcFD path to the MATLAB environment
 addpath('../calcFD')
-r = 2.^[0:4]; % box kernel size
+% define desired box kernel sizes
+r = 2.^[0:4]; % i.e., [1,2,4,8,16]
+% determine the 'counts' using the dilation algorithm
 n = calcFD_dilate(vol,r); 
+% perform a simple linear regression of the log2-transformed box sizes and log2-transformed counts
 c = [log2(r)' ones(length(r),1)] \ -log2(n)'; 
+% take the coefficient corresponding to the slope as the fractal dimensionality (fd) value
 fd = c(1);
 
 %%
